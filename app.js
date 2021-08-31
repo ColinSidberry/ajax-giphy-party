be4b9bd902201355595b9798363f4d816f7b00a7
Colin Sidberry <colinsidberry@colins-mbp.lan>
2021-08-31 16:11:29 -0700

    finished part 1

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 console.log("Let's get this party started!");
+
+
+let $button = $("#submitButton").on("click", handleClick);
+// let $button = $("#submitButton").submit(handleClick); //Question: how do we do the "submit" one?
+
+async function handleClick () {
+    console.log("button submission captured");
+    let $giphySearch = $("#giphySearch").val();
+    console.log("$giphySearch: ", $giphySearch);
+    let response = await axios.get("http://api.giphy.com/v1/gifs/search", {params: {q:$giphySearch,api_key:"MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym"}});
+    // let response = await axios.get("http://api.giphy.com/v1/gifs/search?", {q:$giphySearch});
+    console.log(response);
+};
